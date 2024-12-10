378910ecac25e7e0c6dfe27b37882046500f0808
Anna <7554203@gmail.com>
2024-12-10 22:36:41 +0300

    Получение данных из формы

---
+++ app1.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["get", "post"]) #127.0.0.1:5000/
+def index():
+    if request.method == "POST":
+        area = request.form.get("area")
+        print(area)
+    return render_template("index1.html")
+
+
+if __name__== "__main__":
+    app.run()
