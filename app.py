ae486cb985037b2a999154e7d3cd0c81fab92092
Anna <7554203@gmail.com>
2024-12-10 19:59:27 +0300

    Добавлена html страница

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/") #127.0.0.1:5000/
 def index():
-    return "Hello_World!"
+    return render_template("index.html")
 
 
 if __name__== "__main__":
