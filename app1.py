64c1fab3eb8f90d67740c69961a9dcc562025a6e
Anna <7554203@gmail.com>
2024-12-10 23:47:16 +0300

    Косметические правки

--- app1.py
+++ app1.py
@@ -6,10 +6,11 @@ app = Flask(__name__)
 
 @app.route("/", methods=["get", "post"]) #127.0.0.1:5000/
 def index():
+    message = ""
     if request.method == "POST":
         area = request.form.get("area")
-        print(area)
-    return render_template("index1.html")
+        message = f"Введено что-то нужное: {area}"
+    return render_template("index1.html", message = message)
 
 
 if __name__== "__main__":
