36666de3efda917774acfd10367e5e3710f46b50
Anna <7554203@gmail.com>
2024-12-20 17:02:10 +0300

    приложение

--- processing.py
+++ processing.py
@@ -4,13 +4,22 @@
 #    return cost
 
 def get_prediction(area):
-       if (match('41', value) or match('42', value) or match('43', value)) and  value not in ('43.9', '42.9', '42.2', '43.2', '41.1', '71.1', '43.2'):  
-               return 'Строительно-монтажные работы'
-        if area =  '41.1':  
-                print("Проектно-изыскательские работы")
-        elif area in ('43.2'):  
+#    COEF = 500_000
+#    cost = area * 100
+#    return cost
+        if area == 41:
+                return 'Строительно-монтажные работы'
+        if area == 42:
+                return 'Строительно-монтажные работы'
+        if area == 43:
+                return 'Строительно-монтажные работы'
+        if area == 41.1:
+                return 'Проектно-изыскательские работы'
+        if area == 71.1:
+                return 'Проектно-изыскательские работы'
+        if area == 43.2:
                 return 'Подключение коммуникаций'
-        elif area in ('43.9', '42.9', '42.2'):  
+        if area == 43.9:
                 return 'Строительный надзор'
         else:
-                return 'Прочие'
+                return cost
