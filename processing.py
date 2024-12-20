b14a14c487e1a31f4d211b00c058c05a901d9971
Anna <7554203@gmail.com>
2024-12-20 15:20:39 +0300

    итоговые изменения

--- processing.py
+++ processing.py
@@ -1,5 +1,16 @@
-def get_prediction(area):
-    COEF = 500_000
-    cost = area * COEF
-    return cost
+#def get_prediction(area):
+#    COEF = 500_000
+#    cost = area * COEF
+#    return cost
 
+def get_prediction(area):
+       if (match('41', value) or match('42', value) or match('43', value)) and  value not in ('43.9', '42.9', '42.2', '43.2', '41.1', '71.1', '43.2'):  
+               return 'Строительно-монтажные работы'
+        if area =  '41.1':  
+                print("Проектно-изыскательские работы")
+        elif area in ('43.2'):  
+                return 'Подключение коммуникаций'
+        elif area in ('43.9', '42.9', '42.2'):  
+                return 'Строительный надзор'
+        else:
+                return 'Прочие'
